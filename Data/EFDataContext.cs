50359d95d1abea29713c1049c29e0914ab7f1798
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-12-28 00:23:30 +0000

    chore: Entity Framework setup

---
+++ Data/EFDataContext.cs
@@ -0,0 +1,42 @@
+using Api_Tutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Tutorial.Data
+{
+    public class EFDataContext : DbContext
+    {
+        private readonly IConfiguration _config;
+        public EFDataContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserSalary> UserSalary { get; set; }
+        public virtual DbSet<UserJobInfo> UserJobInfo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(_config.GetConnectionString("DefaultConnection"), optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("TutorialAppSchema");
+
+            modelBuilder.Entity<User>()
+            .ToTable("Users", "TutorialAppSchema")
+            .HasKey(u => u.UserId);
+
+            modelBuilder.Entity<UserSalary>()
+            .HasKey(u => u.UserId);
+
+            modelBuilder.Entity<UserJobInfo>()
+            .HasKey(u => u.UserId);
+        }
+    }
+}
--- obj/Debug/net8.0/api-tutorial.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-tutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b77cd09788f16b455d9d41f76660ed7f6e77b5bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70700e6413359c786a46e1dd1192c66837c687b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
